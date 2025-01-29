be203180ebc961881f3b6d88a0c4e2e5ca858d48
AutumnAurelium <88015631+AutumnAurelium@users.noreply.github.com>
2025-01-28 22:36:09 -0500

    inspector

--- inspect_completions.py
+++ inspect_completions.py
@@ -3,4 +3,6 @@ import json
 with open("completions.jsonl") as f:
     completions = [json.loads(x) for x in f.readlines()]
 
+print(completions[-1]["prompt"][-1][0]["content"])
+print("\n#### RESPONSE\n")
 print(completions[-1]["completions"][-1][0]["content"])
